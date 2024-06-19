3248e0219c0f368e5c2b720f84aaddc49239e731
Nikhil Chhipa <chhipanikhil9@gmail.com>
2024-06-19 16:26:36 +0530

    Create prims_algo.cpp

---
+++ prims_algo.cpp
@@ -0,0 +1,26 @@
+int spanningTree(int V, vector<vector<int>> adj[]){
+  
+    priority_queue<pii,vector<pii>,greater<pii>> q;
+    vector<bool> vis(V,false);
+    q.push({0,0});
+    int sum = 0;
+    
+    while(!q.empty()){
+        int d = q.top().first;
+        int cur = q.top().second;
+        q.pop();
+        
+        if(vis[cur]==1) continue;
+        
+        // add it to the mst
+        vis[cur] = 1;
+        sum += d;
+        
+        for(auto nbr:adj[cur]){
+            if(!vis[nbr[0]]){
+                q.push({nbr[1],nbr[0]});
+            }
+        }
+    }
+    return sum;
+}
