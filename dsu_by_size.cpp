4683c66877e3c1d7b8b8ca335d8b37047e91d926
Nikhil Chhipa <chhipanikhil9@gmail.com>
2024-06-20 14:22:30 +0530

    Update dsu_by_size.cpp

--- dsu_by_size.cpp
+++ dsu_by_size.cpp
@@ -1,3 +1,36 @@
+//In class form
+class DisjointSet{
+   vector<int> sz;
+   vector<int> parent;
+   public:
+   
+   DisjointSet(int n){
+       parent.assign(n+1,0);
+       sz.assign(n+1,1);
+       for(int i=0;i<n;i++) {
+            parent[i] = i;
+            sz[i] = 1;    
+       }
+   }
+   
+   int findParent(int u){
+       if(parent[u]==u)
+            return u;
+        return parent[u] = findParent(parent[u]);
+   }
+   
+   void unionBySize(int u,int v){
+       int ur = findParent(u);
+       int vr = findParent(v);
+       if(ur==vr) return;
+       if(sz[ur]<sz[vr]) swap(ur,vr);// always make ur>vr so ur can be parent of vr
+        parent[vr] = ur;
+        sz[ur] += sz[vr];
+   }
+};
+
+
+/* without class
 vector<int> parent;// resize them in main
  vector<int> sz;
  void make(int u){
@@ -19,37 +52,7 @@ vector<int> parent;// resize them in main
      sz[ur] += sz[vr];
  }
 
-
-/* In class form
-class DisjointSet{
-   public:
-   
-   vector<int> size;
-   vector<int> parent;
-   
-   DisjointSet(int n){
-       parent.assign(n+1,0);
-       size.assign(n+1,1);
-       for(int i=0;i<n;i++) {
-            parent[i] = i;
-            size[i] = 1;    
-       }
-   }
-   
-   int findParent(int u){
-       if(parent[u]==u)
-            return u;
-        return parent[u] = findParent(parent[u]);
-   }
-   
-   void unionBySize(int u,int v){
-       int a = findParent(u);
-       int b = findParent(v);
-       if(a==b) return;
-       
-       if(a<b) swap(a,b);// always make a>b so a can be parent of b
-        parent[b] = a;
-        size[a] += size[b];
-   }
-};
 */
+
+
+
